8fc930dde472636c8863bd534e6bbd90c33de1bb
lucas-mior <lucas.mior@tutamail.com>
2024-09-16 16:06:54 -0300

    remove read_dir.h from functions.h

--- src/functions.h
+++ src/functions.h
@@ -4330,27 +4330,7 @@ void bvshoot(double *y, double *yend, double err, double eps, int32 maxit,
              int32 ivar, double sect);
 
 #endif
-#ifndef read_dir_h
-#define read_dir_h
 
-typedef struct {
-    char **dirnames, **filenames;
-    int32 nfiles, ndirs;
-} FILEINFO;
-
-void free_finfo(FILEINFO *ff);
-int32 cmpstringp(const void *p1, const void *p2);
-int32 get_fileinfo_tab(char *wild, char *direct, FILEINFO *ff, char *wild2);
-int32 get_fileinfo(char *wild, char *direct, FILEINFO *ff);
-int32 fil_count(char *direct, int32 *ndir, int32 *nfil, char *wild, int32 *mld,
-                int32 *mlf);
-int32 change_directory(char *path);
-int32 get_directory(char *direct);
-int32 IsDirectory(char *root, char *path);
-void MakeFullPath(char *root, char *filename, char *pathname);
-int32 wild_match(char *string, char *pattern);
-
-#endif
 #ifndef rubber_h
 #define rubber_h
 
