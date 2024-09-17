4360b8067279e71e1c4e2eb03d7caf81cd45468a
lucas-mior <lucas.mior@tutamail.com>
2024-09-17 20:43:21 -0300

    #include guards

--- src/autlim.h
+++ src/autlim.h
@@ -1,6 +1,11 @@
+#ifndef AUTLIM_H
+#define AUTLIM_H
+
 /* change 200 to whatever to get the dimensions right */
 #define NAUTO 400
 #define N3AUTO (3 * NAUTO + 2)
 #define NxNAUTO (NAUTO * NAUTO)
 #define NPAUTO (NAUTO * 20)
 #define NFAUTO (NAUTO * (2 * NAUTO + 20))
+
+#endif
