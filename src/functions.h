c01a71d8aab4b47eca920dd1e301e1a15ddd6acd
lucas-mior <lucas.mior@tutamail.com>
2024-09-18 16:52:52 -0300

    WARNING: reorganize functions.h order

--- src/functions.h
+++ src/functions.h
@@ -1113,6 +1113,79 @@ uint32 ColorMap(int32 i);
 
 #endif
 
+#ifndef GGETS_H
+#define GGETS_H
+
+#include <X11/Xlib.h>
+
+#define MAX_INCLUDE_FILES 10
+#define ClickTime 200
+
+void ping(void);
+void reset_graphics(void);
+void blank_screen(Window w);
+void set_fore(void);
+void set_back(void);
+void showchar(int32 ch, int32 col, int32 row, Window or);
+void chk_xor(void);
+void set_gcurs(int32 y, int32 x);
+void clr_command(void);
+void draw_info_pop(Window win);
+void bottom_msg(char *msg);
+void gputs(char *string, Window win);
+void err_msg(char *string);
+int32 plintf(char *fmt, ...);
+int32 show_position(XEvent ev);
+void put_command(char *string);
+int32 get_key_press(XEvent *ev);
+void cput_text(void);
+int32 get_mouse_xy(int32 *x, int32 *y, Window w);
+void Ftext(int32 x, int32 y, char *string, Window o);
+void bar(int32 x, int32 y, int32 x2, int32 y2, Window w);
+void rectangle(int32 x, int32 y, int32 x2, int32 y2, Window w);
+void setfillstyle(int32 type, int32 color);
+void circle(int32 x, int32 y, int32 radius, Window w);
+void xline(int32 x0, int32 y0, int32 x1, int32 y1, Window w);
+int32 new_float(char *name, double *value);
+int32 new_int(char *name, int32 *value);
+void display_command(char *name, char *value, int32 pos);
+void clr_line_at(Window w, int32 col0, int32 pos, int32 n);
+void put_cursor_at(Window w, int32 col0, int32 pos);
+void put_string_at(Window w, int32 col, char *s, int32 off);
+void movmem(char *s1, char *s2, int32 len);
+void memmov(char *s1, char *s2, int32 len);
+void edit_window(Window w, int32 *pos, char *value, int32 *col, int32 *done,
+                 int32 ch);
+void do_backspace(int32 *pos, char *value, int32 *col, Window w);
+void edit_command_string(XEvent ev, char *name, char *value, int32 *done,
+                         int32 *pos, int32 *col);
+int32 new_string(char *name, char *value);
+
+#endif
+
+#ifndef LOAD_EQN_H
+#define LOAD_EQN_H
+
+#include <stdio.h>
+
+/*
+The acutual max filename length is determined by the
+FILENAME_MAX (see <stdio.h>), and usually 4096 -- but
+this is huge and usually overkill.  On the otherhand
+the old Xpp default string buffer size of 100 is a bit
+restricitive for lengths of filenames. You could also
+set this define in the Makefile or at compile time to
+override the below definition.
+*/
+
+#ifndef XPP_MAX_NAME
+#define XPP_MAX_NAME 300
+#if (XPP_MAX_NAME > FILENAME_MAX)
+#define XPP_MAX_NAME FILENAME_MAX
+#endif
+
+#endif
+
 #ifndef COMLINE_H
 #define COMLINE_H
 
@@ -1133,6 +1206,8 @@ extern int32 loadincludefile;
 extern int32 querysets, querypars, queryics, dryrun;
 extern int32 noicon;
 extern int32 newseed;
+
+extern char includefilename[MAX_INCLUDE_FILES][XPP_MAX_NAME];
 
 int32 is_set_name(SET_NAME *set, char *nam);
 SET_NAME *add_set(SET_NAME *set, char *nam);
@@ -2590,56 +2665,6 @@ void save_batch_shoot(void);
 
 #endif
 
-#ifndef GGETS_H
-#define GGETS_H
-
-#include <X11/Xlib.h>
-
-#define MAX_INCLUDE_FILES 10
-#define ClickTime 200
-
-void ping(void);
-void reset_graphics(void);
-void blank_screen(Window w);
-void set_fore(void);
-void set_back(void);
-void showchar(int32 ch, int32 col, int32 row, Window or);
-void chk_xor(void);
-void set_gcurs(int32 y, int32 x);
-void clr_command(void);
-void draw_info_pop(Window win);
-void bottom_msg(char *msg);
-void gputs(char *string, Window win);
-void err_msg(char *string);
-int32 plintf(char *fmt, ...);
-int32 show_position(XEvent ev);
-void put_command(char *string);
-int32 get_key_press(XEvent *ev);
-void cput_text(void);
-int32 get_mouse_xy(int32 *x, int32 *y, Window w);
-void Ftext(int32 x, int32 y, char *string, Window o);
-void bar(int32 x, int32 y, int32 x2, int32 y2, Window w);
-void rectangle(int32 x, int32 y, int32 x2, int32 y2, Window w);
-void setfillstyle(int32 type, int32 color);
-void circle(int32 x, int32 y, int32 radius, Window w);
-void xline(int32 x0, int32 y0, int32 x1, int32 y1, Window w);
-int32 new_float(char *name, double *value);
-int32 new_int(char *name, int32 *value);
-void display_command(char *name, char *value, int32 pos);
-void clr_line_at(Window w, int32 col0, int32 pos, int32 n);
-void put_cursor_at(Window w, int32 col0, int32 pos);
-void put_string_at(Window w, int32 col, char *s, int32 off);
-void movmem(char *s1, char *s2, int32 len);
-void memmov(char *s1, char *s2, int32 len);
-void edit_window(Window w, int32 *pos, char *value, int32 *col, int32 *done,
-                 int32 ch);
-void do_backspace(int32 *pos, char *value, int32 *col, Window w);
-void edit_command_string(XEvent ev, char *name, char *value, int32 *done,
-                         int32 *pos, int32 *col);
-int32 new_string(char *name, char *value);
-
-#endif
-
 int32 go_go_auto(void);
 
 #ifndef GRAF_PAR_H
@@ -3402,29 +3427,6 @@ double RPowerR(double base, double exponent);
  ******************************************************************/
 
 double RSqrt(double x);
-
-#endif
-
-#ifndef LOAD_EQN_H
-#define LOAD_EQN_H
-
-#include <stdio.h>
-
-/*
-The acutual max filename length is determined by the
-FILENAME_MAX (see <stdio.h>), and usually 4096 -- but
-this is huge and usually overkill.  On the otherhand
-the old Xpp default string buffer size of 100 is a bit
-restricitive for lengths of filenames. You could also
-set this define in the Makefile or at compile time to
-override the below definition.
-*/
-
-#ifndef XPP_MAX_NAME
-#define XPP_MAX_NAME 300
-#if (XPP_MAX_NAME > FILENAME_MAX)
-#define XPP_MAX_NAME FILENAME_MAX
-#endif
 
 #endif
 
