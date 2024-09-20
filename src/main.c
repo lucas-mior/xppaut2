b30e098b5c32729e1ed59e67966729be46d038c2
lucas-mior <lucas.mior@tutamail.com>
2024-09-20 16:42:58 -0300

    abort.c: pass code to main.c

--- src/main.c
+++ src/main.c
@@ -144,6 +144,48 @@ XFontStruct *big_font, *small_font;
 int32 popped = 0;
 
 void
+plot_command(int32 nit, int32 icount, int32 cwidth) {
+    double dx;
+
+    if (nit == 0)
+        return;
+
+    dx = (double)icount*(double)cwidth / (double)nit;
+    XDrawPoint(display, command_pop, gc, (int32)dx, 5);
+    return;
+}
+
+int32
+my_abort(void) {
+    int32 ch;
+
+    while (XPending(display) > 0) {
+        XEvent event;
+        XNextEvent(display, &event);
+
+        if (check_ani_pause(event) == 27)
+            return 27;
+
+        switch (event.type) {
+        case Expose:
+            do_expose(event);
+            break;
+        case ButtonPress:
+            break;
+        case KeyPress:
+            ch = get_key_press(&event);
+            return ch;
+        default:
+            break;
+        }
+
+        return 0;
+    }
+
+    return 64;
+}
+
+void
 do_main(int32 argc, char **argv) {
     char myfile[XPP_MAX_NAME];
     char pptitle[80];
