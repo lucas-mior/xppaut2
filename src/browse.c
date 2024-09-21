cd1641e5d9038a8cd694fa91bfdbed65f70c2ba3
lucas-mior <lucas.mior@tutamail.com>
2024-09-21 18:33:26 -0300

    fix leftover format string warning

--- src/browse.c
+++ src/browse.c
@@ -1331,8 +1331,8 @@ data_table(Browser *b) {
     strncpy(value[0], uvar_names[0], sizeof(value[0]));
     snprintf(value[1], sizeof(value[1]), "0.00");
     snprintf(value[2], sizeof(value[2]), "1.00");
-    snprintf(value[3], sizeof(value[3]), "%.*s.tab", (int)sizeof(value[0]),
-             value[0]);
+    snprintf(value[3], sizeof(value[3]),
+             "%.*s.tab", (int)sizeof(value[0]) - 5, value[0]);
 
     XGetInputFocus(display, &w, &rev);
     status = do_string_box(4, 4, 1, "Tabulate", name, value, 40);
