f4fc9e6a940b1c021964e5467e9f7f940d88bc8c
lucas-mior <lucas.mior@tutamail.com>
2024-09-23 11:57:29 -0300

    wip

--- commas.sh
+++ commas.sh
@@ -3,16 +3,17 @@
 files="src/*.c src/cuda/*.c src/cvode/*.c src/sbml/*.c"
 grep -E -l "^[a-zA-Z0-9_]+ (\*?[a-zA-Z0-9_]+, ?)+\*?[a-zA-Z0-9_]+;$" $files \
     | while read file; do
-awk '/^[a-zA-Z0-9_]+ (*?[a-zA-Z0-9_]+, ?)+*?[a-zA-Z0-9_]+;$/ {
-type = $1
-$1 = "";
-for (i = 2; i <= NF; i += 1) {
-    var[i] = gensub("(*?[a-zA-Z0-9_]+)[,;] ?", "\\1", "g", $i);
-    printf("%s %s;\n", type, var[i]);
+awk '
+/^[a-zA-Z0-9_]+ (*?[a-zA-Z0-9_]+, ?)+*?[a-zA-Z0-9_]+;$/ {
+    type = $1
+    $1 = "";
+    for (i = 2; i <= NF; i += 1) {
+        var = gensub("(*?[a-zA-Z0-9_]+)[,;] ?", "\\1", "g", $i);
+        printf("%s %s;NEWLINE", type, var);
+    }
 }
 {
     print
-}
 }' "$file" > "${file}.2"
 mv "${file}.2" "$file"
 done
