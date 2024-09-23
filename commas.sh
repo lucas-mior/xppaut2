a88a294a89385f2d911a4e1225830a8fe8e2e9fa
lucas-mior <lucas.mior@tutamail.com>
2024-09-23 13:31:39 -0300

    save and wip

--- commas.sh
+++ commas.sh
@@ -2,10 +2,10 @@
 
 find src -iname "*.[ch]" | while read file; do
 awk '
-/^[[:alnum:]_]+ (\*?[[:alnum:]_]+(\[[^]]\])?, )+(\*?[[:alnum:]_]+(\[[^]]\])?);$/ {
+/^[[:alnum:]_]+ (\*?[[:alnum:]_]+(\\[[^]]\\])?, )+(\*?[[:alnum:]_]+(\\[[^]]\\])?);$/ {
     type = $1
     for (i = 2; i <= NF; i += 1) {
-        var = gensub("(\*?[[:alnum:]_](\[[^]]\])?+)[,;]", "\\1", "g", $i);
+        var = gensub("(\*?[[:alnum:]_](\\[[^]]\\])?+)[,;]", "\\1", "g", $i);
         printf("%s %s;NEWLINELINE", type, var);
     }
     getline
