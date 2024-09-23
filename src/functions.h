40ced2f86cf25f434aff3a1fb98225a4eb8e8426
lucas-mior <lucas.mior@tutamail.com>
2024-09-23 08:13:50 -0300

    ani: lots of functions private

--- src/functions.h
+++ src/functions.h
@@ -116,79 +116,11 @@ extern int32 animation_on_the_fly;
 void ani_new_vcr(void);
 void ani_do_events(XEvent ev);
 void ani_expose(Window window);
-void ani_newskip(void);
-void check_on_the_fly(void);
 void on_the_fly(int32 task);
-void ani_frame(int32 task);
-void set_to_init_data(void);
-void set_from_init_data(void);
-void ani_flip1(int32 n);
-void ani_flip(void);
-void ani_disk_warn(void);
 int32 getppmbits(Window window, int32 *wid, int32 *hgt, uchar *out);
 int32 writeframe(char *filename, Window window, int32 wid, int32 hgt);
 void ani_zero(void);
 void get_ani_file(char *fname);
-int32 ani_new_file(char *filename);
-int32 load_ani_file(FILE *fp);
-int32 parse_ani_string(char *s, FILE *fp);
-void set_ani_dimension(char *x1, char *y1, char *x2, char *y2);
-int32 add_ani_com(int32 type, char *x1, char *y1, char *x2, char *y2, char *col,
-                  char *thick);
-void init_ani_stuff(void);
-void free_ani(void);
-int32 chk_ani_color(char *s, int32 *index);
-int32 add_ani_expr(char *x, int32 *c);
-int32 add_ani_rline(AniCom *a, char *x1, char *y1, char *col, char *thick);
-void reset_comets(void);
-void roll_comet(AniCom *a, int32 xn, int32 yn, int32 col);
-int32 add_ani_comet(AniCom *a, char *x1, char *y1, char *x2, char *col,
-                    char *thick);
-int32 add_ani_line(AniCom *a, char *x1, char *y1, char *x2, char *y2, char *col,
-                   char *thick);
-int32 add_ani_null(AniCom *a, char *x1, char *y1, char *x2, char *y2, char *col,
-                   char *who);
-int32 add_ani_rect(AniCom *a, char *x1, char *y1, char *x2, char *y2, char *col,
-                   char *thick);
-int32 add_ani_frect(AniCom *a, char *x1, char *y1, char *x2, char *y2,
-                    char *col, char *thick);
-int32 add_ani_ellip(AniCom *a, char *x1, char *y1, char *x2, char *y2,
-                    char *col, char *thick);
-int32 add_ani_fellip(AniCom *a, char *x1, char *y1, char *x2, char *y2,
-                     char *col, char *thick);
-int32 add_ani_circle(AniCom *a, char *x1, char *y1, char *x2, char *col,
-                     char *thick);
-int32 add_ani_fcircle(AniCom *a, char *x1, char *y1, char *x2, char *col,
-                      char *thick);
-int32 add_ani_text(AniCom *a, char *x1, char *y1, char *y2);
-int32 add_ani_vtext(AniCom *a, char *x1, char *y1, char *x2, char *y2);
-int32 add_ani_settext(AniCom *a, char *x1, char *y1, char *col);
-void render_ani(void);
-void set_ani_perm(void);
-void eval_ani_color(int32 j);
-void eval_ani_com(int32 j);
-void set_ani_thick(int32 t);
-void set_ani_font_stuff(int32 size, int32 font, int32 color);
-void set_ani_col(int32 j);
-void xset_ani_col(int32 icol);
-void ani_rad2scale(double rx, double ry, int32 *ix, int32 *iy);
-void ani_radscale(double rad, int32 *ix, int32 *iy);
-void ani_ij_to_xy(int32 ix, int32 iy, double *x, double *y);
-void ani_xyscale(double x, double y, int32 *ix, int32 *iy);
-void draw_ani_comet(int32 j);
-void draw_ani_null(int32 j, int32 id);
-void draw_ani_line(int32 j);
-void draw_ani_rline(int32 j);
-void draw_ani_circ(int32 j);
-void draw_ani_fcirc(int32 j);
-void draw_ani_rect(int32 j);
-void draw_ani_frect(int32 j);
-void draw_ani_ellip(int32 j);
-void draw_ani_fellip(int32 j);
-void draw_ani_text(int32 j);
-void draw_ani_vtext(int32 j);
-void tst_pix_draw(void);
-void read_ani_line(FILE *fp, char *s);
 void de_space(char *s);
 int32 add_grab_command(char *xs, char *ys, char *ts, FILE *fp);
 void info_grab_stuff(void);
