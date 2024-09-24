126dfe49165dfa1412506c10ce93c0f6cfb03708
lucas-mior <lucas.mior@tutamail.com>
2024-09-24 10:57:24 -0300

    prepare renames.sh

--- renames.sh
+++ renames.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 files="src/*.c src/cuda/*.c src/cvode/*.c src/sbml/*.c"
-grep -E "^extern [[:alnum:]_]+ \*?[[:alnum:]_]+\\[\\];$" $files \
+grep -E "^extern [[:alnum:]_]+ \*?[[:alnum:]_]+\\[[^]]+\\];$" $files \
     | while read match; do
         type="$(awk '{print $2}' <<< "$match")"
         file="$(awk '{file = gensub("([[:alnum:]_]+):.+", "\\1", "g", $1); print file}' <<< "$match")"
