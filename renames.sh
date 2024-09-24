574b839c3104571c7429499034b7a6df880d4cc6
lucas-mior <lucas.mior@tutamail.com>
2024-09-24 10:28:31 -0300

    prepare renames.sh

--- renames.sh
+++ renames.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-grep -E -R -l "^extern [[:alnum:]_]+ [[:alnum:]_]+;$"
+grep -E -R "^extern [[:alnum:]_]+ [[:alnum:]_]+;$" \
+    | while read match; do
+        type="$(awk '{print $2}' <<< "$match")"
+        file="$(awk '{file = gensub("([[:alnum:]_]+):.+", "\\1", "g", $1); print file}' <<< "$match")"
+        name="$(awk '{print $3}' <<< "$match")"
+        grep -q "^$type $name" "$file" || sed -i "/^$type $name/d" "$file"
+    done
