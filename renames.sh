c8ac04637d294db94b494b8531eaa31ef8425cb0
lucas-mior <lucas.mior@tutamail.com>
2024-09-24 07:30:54 -0300

    rename.sh

--- renames.sh
+++ renames.sh
@@ -5,6 +5,5 @@ grep -E "^[[:alnum:]_]+ \**[[:alnum:]_]+\([^)]+\);$" "src/functions.h" \
     | sed -E 's|\*|\\*|g' \
     | while read function; do
     file="$(grep -E -l "^${function}\(.+\) {$" src/*.c | sed 's/\.c$//; s|src/||')"
-    grep -E -q "_" <<< "$function" || echo "$function $file"
-    # echo "$function $function"
+    grep -E -q "$file" <<< "$function" || echo "$function $file"
 done
