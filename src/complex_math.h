4a4a3f1494f8bd91766deaa471278a8b8f641c24
lucas-mior <lucas.mior@tutamail.com>
2024-09-24 14:57:24 -0300

    start putting stuff in complex_math.h

---
+++ src/complex_math.h
@@ -0,0 +1,15 @@
+#ifndef COMPLEX_MATH_H
+#define COMPLEX_MATH_H
+
+#include "integers.h"
+
+typedef struct {
+    double r;
+    double i;
+} floatcomplex;
+typedef struct {
+    double r;
+    double i;
+} doublecomplex;
+
+#endif
