2b607056a83661a4f74371a8a97729e4cefe2c8b
lucas-mior <lucas.mior@tutamail.com>
2024-09-24 15:52:32 -0300

    use libc lgamma(double)

--- src/parserslow.c
+++ src/parserslow.c
@@ -30,10 +30,6 @@ static double zippy;
         stack[stack_pointer++] = zippy;                                        \
     } while (0)
 
-#ifdef NOLGAMMA
-double lgamma();
-#endif
-
 /* #define COM(a) my_symb[toklist[(a)]].com */
 int32 ERROUT;
 int32 NDELAYS = 0;
@@ -2193,26 +2189,6 @@ eval_rpn(int32 *equat) {
 }
 
 /* code for log-gamma if you dont have it */
-
-#ifdef NOLGAMMA
-double
-lgamma(double xx) {
-    double x, y, tmp, ser;
-    static double cof[6] = {76.18009172947146,     -86.50532032941677,
-                            24.01409824083091,     -1.231739572450155,
-                            0.1208650973866179e-2, -0.5395239384953e-5};
-    int32 j;
-
-    y = x = xx;
-    tmp = x + 5.5;
-    tmp -= (x + 0.5)*log(tmp);
-    ser = 1.000000000190015;
-    for (j = 0; j <= 5; j++)
-        ser += cof[j] / ++y;
-    return -tmp + log(2.5066282746310005*ser / x);
-}
-
-#endif
 
 /*  STRING STUFF  */
 #ifndef STRUPR
