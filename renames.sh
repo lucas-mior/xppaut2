bc259fef535ff1599199714b7876cd30ade2ec06
lucas-mior <lucas.mior@tutamail.com>
2024-09-24 10:28:59 -0300

    prepare renames.sh

--- renames.sh
+++ renames.sh
@@ -5,5 +5,5 @@ grep -E -R "^extern [[:alnum:]_]+ [[:alnum:]_]+;$" \
         type="$(awk '{print $2}' <<< "$match")"
         file="$(awk '{file = gensub("([[:alnum:]_]+):.+", "\\1", "g", $1); print file}' <<< "$match")"
         name="$(awk '{print $3}' <<< "$match")"
-        grep -q "^$type $name" "$file" || sed -i "/^$type $name/d" "$file"
+        grep -q "^$type $name" "$file" || sed -i "/^extern $type $name/d" "$file"
     done
