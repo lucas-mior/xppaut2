ac6a3de112c11910b9ac34b4638c8ba74098e224
lucas-mior <lucas.mior@tutamail.com>
2024-09-25 08:10:22 -0300

    del_stab: don't use dangerous macro

--- src/del_stab.c
+++ src/del_stab.c
@@ -178,13 +178,11 @@ void
 switch_rows(COMPLEX *z, int32 i1, int32 i2, int32 n) {
     COMPLEX zt;
     int32 j;
-#define Z(a, b) z[(a) + n*(b)]
     for (j = 0; j < n; j++) {
-        zt = Z(i1, j);
-        Z(i1, j) = Z(i2, j);
-        Z(i2, j) = zt;
+        zt = z[(i1) + n*(j)];
+        z[(i1) + n*(j)] = z[(i2) + n*(j)];
+        z[(i2) + n*(j)] = zt;
     }
-#undef Z
     return;
 }
 
@@ -207,11 +205,10 @@ cdeterm(COMPLEX *z, int32 n) {
     double q;
     double qmax;
     COMPLEX sign = rtoc(1.0, 0.0), mult, sum, zd;
-#define Z(a, b) z[(a) + n*(b)]
     for (j = 0; j < n; j++) {
         qmax = 0.0;
         for (i = j; i < n; i++) {
-            q = c_abs(Z(i, j));
+            q = c_abs(z[(i) + n*(j)]);
             if (q > qmax) {
                 qmax = q;
                 imax = i;
@@ -222,19 +219,18 @@ cdeterm(COMPLEX *z, int32 n) {
         switch_rows(z, imax, j, n);
         if (imax > j)
             sign = cmlt(rtoc(-1.0, 0.0), sign);
-        zd = Z(j, j);
+        zd = z[(j) + n*(j)];
         for (i = j + 1; i < n; i++) {
-            mult = cdivv(Z(i, j), zd);
+            mult = cdivv(z[(i) + n*(j)], zd);
             for (k = j + 1; k < n; k++) {
-                Z(i, k) = cdif(Z(i, k), cmlt(mult, Z(j, k)));
+                z[(i) + n*(k)] = cdif(z[(i) + n*(k)], cmlt(mult, z[(j) + n*(k)]));
             }
         }
     }
     sum = sign;
     for (j = 0; j < n; j++)
-        sum = cmlt(sum, Z(j, j));
+        sum = cmlt(sum, z[(j) + n*(j)]);
     return sum;
-#undef Z
 }
 
 void
