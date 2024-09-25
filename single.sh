e429e76fb0c9bbe659a6974370d73df4ed2376c7
lucas-mior <lucas.mior@tutamail.com>
2024-09-25 14:06:17 -0300

    update single.sh

--- single.sh
+++ single.sh
@@ -3,7 +3,7 @@
 FUNC_NAME="\**[[:alnum:]_]+"
 files="src/*.[ch] src/cvode/*.[ch]"
 
-grep -E "^$FUNC_NAME\(" $files \
+grep -E "^$FUNC_NAME\(void\)" $files \
 | while read match; do
     file="${match%:*}"
     function="${match##*:}"
@@ -11,8 +11,7 @@ grep -E "^$FUNC_NAME\(" $files \
 
     n="$(grep -F -l "$function" $files | wc -l)"
     if [ $n -eq 2 ]; then
-        # m="$(grep -F "$function" "$file" | wc -l)"
-        # [ $m -le 2 ] && echo "${function%(*}"
-        echo "$function"
+        m="$(grep -F "$function" "$file" | wc -l)"
+        [ $m -le 2 ] && echo "$function"
     fi
 done
