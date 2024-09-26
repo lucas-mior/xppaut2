180175f580869caa25083666d3cdfd3e67c8256d
lucas-mior <lucas.mior@tutamail.com>
2024-09-26 09:04:58 -0300

    organize main.c

--- src/main.c
+++ src/main.c
@@ -1,42 +1,39 @@
-#include "functions.h"
-#include "integers.h"
-#include <stdbool.h>
-
-#include "read_dir.h"
-#include "auto_nox.h"
-
-#include <stdlib.h>
-
 /*
-    Copyright (C) 2002-2017  Bard Ermentrout & Daniel Dougherty
+ *  Copyright (C) 2002-2017  Bard Ermentrout & Daniel Dougherty
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-    The author can be contacted at
-     bard@pitt.edu
+ *  The author can be contacted at
+ *   bard@pitt.edu
+ */
 
-*/
-
+#include <X11/Xatom.h>
 #include <X11/Xlib.h>
-#include <X11/Xutil.h>
 #include <X11/Xos.h>
 #include <X11/Xproto.h>
-#include <X11/Xatom.h>
-#include "pp.bitmap"
-#include <stdio.h>
+#include <X11/Xutil.h>
 #include <dirent.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "auto_nox.h"
+#include "functions.h"
+#include "integers.h"
+#include "pp.bitmap"
+#include "read_dir.h"
 
 #define FIX_SIZE 3
 #define FIX_MIN_SIZE 2
