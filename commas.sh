9c1224f3dd9e6b6c28ab48e4d98df667026f3f8c
lucas-mior <lucas.mior@tutamail.com>
2024-09-27 09:26:21 -0300

    add commas.sh

---
+++ commas.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# int32 auto_ntst = 15, auto_nmx = 200, auto_npr = 50, auto_ncol = 4;
+IDENT="[[:alnum:]_]+"
+BRACKETS='\[.*\]'
+
+find src -iname "*.[ch]" | while read file; do
+
+awk " /^[[:alnum:]_]+ ($IDENT( = \S+)?, )+$IDENT( = \S+)?;\$/ {
+print; exit
+    type = \$1
+    \$1 = \"\"
+
+    split(\$0, array, \",\");
+    for (i in array) {
+        printf(\"%s %s;NEWLINELINE\", type, array[i]);
+    }
+    getline
+# }{
+#     print
+}" "$file" | tee "${file}.2"
+mv "${file}.2" "$file"
+
+sed -i ':a;N;$!ba; s/NEWLINELINE/\n/g' "$file"
+sed -i 's/,;$/;/' "$file"
+sed -i 's/;;$/;/' "$file"
+done
